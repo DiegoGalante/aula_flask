ddf23355da81796188f08c9e14b53962201e00f8
Diego Galante <diegogalantte@gmail.com>
2018-05-16 18:58:12 -0300

    Aula 5 - Migrações. Instalação do flask-migrate e flask-script, e primeiros comandos com migrations como migrate e upgrade.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+
 db = SQLAlchemy(app)
+#instancia o Migrate
+#E o migrate cuida das migrações e pra isso ele precisa receber, tanto a aplicação quanto o banco de dados
+migrate = Migrate(app, db)
+
+#Manager vai cuidar dos comandos que vou usar na aplicação
+#por padrão o primeiro dela sé o runserver que é o comando de execução da aplicação
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
-
+#from app.models import tables
--- app/models/tables.py
+++ app/models/tables.py
@@ -1,29 +1,22 @@
-#da pasta app, importa o db
 from app import db
 
-
-#db.Model é uma classe do sqlalchemy padrão
-#a User herda a db.Model
 class User(db.Model)
     __tablename__ = "users"
-    #unique significa que não pode existir outro usuario com o mesmo nome
-    #tipo um só e-mail na base de dados, etc
-    #db.String é possível passar o tamanho da string, assim db.String[120], se deixar sem tamanho, vai ser atribuido o tamanho maximo permitido praquele banco
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
 
-    #construtor
+
     def __init__(self, username, password, name, email):
         self.username = username
         self.password = password
         self.name = name
         self.email = email
 
-    #significa representação.
-    #é o jeito que a classe vai aparecer quando fizer uma pesquisa no banco de dados
+
     def __repr__(self):
         return "<User %r>" % self.username
         
@@ -35,7 +28,7 @@ class Post(db.Model):
     content = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    #poderia ser qualquer nome, ao invés de 'user'
+    
     user = db.relationship('User', foreign_keys=user_id)
 
     def __init__(self, content, user_id):
