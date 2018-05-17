faa6da604f929ba5a9671f31d39dcfb7b22453b3
Diego Galante <diegogalantte@gmail.com>
2018-05-17 15:12:31 -0300

    Aula 7 Rotas: Criações de páginas e formas de parametros na URL.

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,20 @@
 from app import app
 
+@app.route("/index")
 @app.route("/")
 def index():
-    return "Hello World";
+    return "Hello World"
+
+@app.route("/test", defaults={'name':None})
+@app.route("/test/<name>")
+def test(name):
+    print(type(name))
+    if name:
+        return "Olá, %s!" % name
+    else:
+        return "Olá, usuario!"
+
+#@app.route("/teste/<int:id>")
+@app.route("/teste/", methods=['GET'])
+def teste():
+    return "Oi!"
