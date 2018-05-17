13b69b406f6f5568ad0bc4a790e15d32d55c35a5
Diego Galante <diegogalantte@gmail.com>
2018-05-17 15:48:50 -0300

    Aula 8 - Templates: Inclusão do arquivo index.html na pasta templates. Inclusão dos arquivos bootstrap css e js na pasta static e a referencia deles no arquivo index.html

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,20 +1,8 @@
+#Vai receber um arquivo html e vai renderizá-lo
+from flask import render_template
 from app import app
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return "Hello World"
-
-@app.route("/test", defaults={'name':None})
-@app.route("/test/<name>")
-def test(name):
-    print(type(name))
-    if name:
-        return "Olá, %s!" % name
-    else:
-        return "Olá, usuario!"
-
-#@app.route("/teste/<int:id>")
-@app.route("/teste/", methods=['GET'])
-def teste():
-    return "Oi!"
+@app.route("/index/<user>")
+@app.route("/", defaults={'user':None})
+def index(user):
+    return render_template('index.html', user=user)
