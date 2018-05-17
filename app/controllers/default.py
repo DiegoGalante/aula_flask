9330274bfc3a05e3a7ebde292865ed8b62817172
Diego Galante <diegogalantte@gmail.com>
2018-05-17 16:27:20 -0300

    Aula 9 Templates - Continuação: Inclusão da página base.html. Divisão do conteúdo das páginas, base.html e index.html.

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,7 +2,12 @@
 from flask import render_template
 from app import app
 
-@app.route("/index/<user>")
-@app.route("/", defaults={'user':None})
-def index(user):
-    return render_template('index.html', user=user)
+@app.route("/index")
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('base.html')
+    
