3e71b8aa527a217276fe09e11fd66ec1342bea01
Diego Galante <diegogalantte@gmail.com>
2018-05-17 14:53:59 -0300

    Aula 6 - Configurações: Separando arquivo de configuração e as atribuindo ao flask.

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,13 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+#forma simples de passar as configurações para o flask
+#e manter as configurações em um arquivo a parte
+app.config.from_object('config')
 
 db = SQLAlchemy(app)
-#instancia o Migrate
-#E o migrate cuida das migrações e pra isso ele precisa receber, tanto a aplicação quanto o banco de dados
 migrate = Migrate(app, db)
 
-#Manager vai cuidar dos comandos que vou usar na aplicação
-#por padrão o primeiro dela sé o runserver que é o comando de execução da aplicação
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
