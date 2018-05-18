a0e64bf15cf61f48a4f8ad0eb7d4410663692ce2
Diego Galante <diegogalantte@gmail.com>
2018-05-18 16:16:21 -0300

    Aula 10 -  Formulários: Construção do formulário de login. Envio dos dados via POST e validação de erros. Obs: Correção do meu erro no arquivo tables.py e importação das tabelas no modulo principal do app.

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 #forma simples de passar as configurações para o flask
 #e manter as configurações em um arquivo a parte
 app.config.from_object('config')
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -15,4 +14,4 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
-#from app.models import tables
+from app.models import tables
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,12 +2,22 @@
 from flask import render_template
 from app import app
 
+from app.models.forms import LoginForm
+
 @app.route("/index")
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template('base.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+        print(form.remember_me.data)
+        pass
+    else:
+        print(form.errors)
+    return render_template('login.html', form=form)
     
